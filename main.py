ff038f3028e54f429307ee630acc1bb6fd16ee1f
curiousdarko <133599091+curiousdarko@users.noreply.github.com>
2023-05-22 18:55:20 +0100

    Create main.py
    thats it

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+def unlock_guns():
+    os.system("cd C:/Program Files (x86)/Activision/Call of Duty Modern Warfare 2/players")
+    os.system("echo \"sv_cheats 1\" > config_mp.cfg")
+    os.system("echo \"give all\" > config_mp.cfg")
+    os.system("echo \"sv_cheats 0\" > config_mp.cfg")
+
+if __name__ == "__main__":
+    unlock_guns()
